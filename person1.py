1e238ee41f6c7aae230779ca108e674bd27766cb
Abhishek723 <abhi95103@gmail.com>
2018-03-08 22:40:39 +0530

    some changes

--- person1.py
+++ person1.py
@@ -5,14 +5,9 @@ chars = '''abcdefghijklmnopqrstuvwxyz0123456789,./;'[]!@#$%^&*()_+~\|:"<>?'''
 times = int(input('[.]Number of passwords?\n[.]'))
 length = int(input('[.]Password length?\n[.]'))
 
-<<<<<<< HEAD
-f = open('path/Kanpur','w')
-=======
 
 f = open('path/delhi','w')
 
-
->>>>>>> 08cc3c972514d3de9bf45371f204ab66a021f901
 
 for i in range(times):
 	password = ''
